77350f0af9025ade3fb2ecf96cb302c6a9a56df6
Zigalow <s224302@dtu.dk>
2024-10-03 22:07:02 +0200

    Moved to correct place

--- Todo.Api/Program.cs
+++ Todo.Api/Program.cs
@@ -8,11 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
-
 builder.Services.AddDbContext<TodoDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())

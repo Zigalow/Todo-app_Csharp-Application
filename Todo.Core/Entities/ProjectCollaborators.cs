215361c118b2096f67e4549bd9b83a094f58d295
Zigalow <s224302@dtu.dk>
2024-10-03 22:06:42 +0200

    There was a problem with this since it consisted of composite keys. Fixed.

--- Todo.Core/Entities/ProjectCollaborators.cs
+++ Todo.Core/Entities/ProjectCollaborators.cs
@@ -1,16 +1,14 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace Todo.Core.Entities;
 
+[PrimaryKey(nameof(UserId), nameof(ProjectId))]
 public class ProjectCollaborators
 {
-    [Key]
-    [Column(Order = 0)]
     public int UserId { get; set; }
 
-    [Key]
-    [Column(Order = 0)]
     public int ProjectId { get; set; }
 
     [Required]

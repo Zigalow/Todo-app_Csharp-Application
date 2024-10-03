c0a8890446894be413f18b155412f5f07c1f5692
Zigalow <s224302@dtu.dk>
2024-10-03 22:06:02 +0200

    Added ValueComparer

--- Todo.Api/Configuration/RoleConfiguration.cs
+++ Todo.Api/Configuration/RoleConfiguration.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Todo.Core.Entities;
 
@@ -8,10 +9,16 @@ public class RoleConfiguration : IEntityTypeConfiguration<Role>
 {
     public void Configure(EntityTypeBuilder<Role> builder)
     {
+        var permissionTypeComparer = new ValueComparer<HashSet<PermissionType>>(
+            (c1, c2) => c1.SetEquals(c2),
+            c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode())),
+            c => new HashSet<PermissionType>(c));
+
         builder.Property(b => b.Permissions)
             .HasConversion(
                 v => string.Join(',', v.Select(p => p.ToString())),
                 v => new HashSet<PermissionType>(v.Split(',', StringSplitOptions.RemoveEmptyEntries)
-                    .Select(Enum.Parse<PermissionType>)));
+                    .Select(Enum.Parse<PermissionType>)))
+            .Metadata.SetValueComparer(permissionTypeComparer);
     }
 }

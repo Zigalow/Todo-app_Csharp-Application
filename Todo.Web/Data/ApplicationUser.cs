90903d6c92ed6d4b4377ed6968c189389bfd3aa9
Zaid Sheikh <Zaidsh02@gmail.com>
2024-10-14 22:26:53 +0200

    Temperory ApplicationUser
    Couldn't access Todo.Core, as I don't have enough space for Visual studios to work
    
    Co-Authored-By: Ali Masoud <96056143+AliTheBoss03@users.noreply.github.com>

--- Todo.Web/Data/ApplicationUser.cs
+++ Todo.Web/Data/ApplicationUser.cs
@@ -1,8 +1,16 @@
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace Todo.Web.Data;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    [Required]
+    [StringLength(100)]
+    public string Name { get; set; } = string.Empty;
+
+    //[Required]
+    //public ICollection<Project> Projects { get; set; } = new List<Project>();
 }

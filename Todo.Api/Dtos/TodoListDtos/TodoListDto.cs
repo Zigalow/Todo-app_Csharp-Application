adaa9ffcb095ebea88abb12f016a67dbe519d2b8
Zigalow <s224302@dtu.dk>
2024-10-15 17:11:05 +0200

    Changed order

--- Todo.Api/Dtos/TodoListDtos/TodoListDto.cs
+++ Todo.Api/Dtos/TodoListDtos/TodoListDto.cs
@@ -4,7 +4,6 @@ public class TodoListDto
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public int ProjectId { get; set; }
-    
     public int TodoCount { get; set; }
+    public int ProjectId { get; set; }
 }

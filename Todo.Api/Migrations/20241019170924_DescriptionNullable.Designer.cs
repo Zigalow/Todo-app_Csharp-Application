dbb4bd77eaa14beb20eb182e0eb0b46d8d5afc38
Zigalow <s224302@dtu.dk>
2024-10-19 19:15:58 +0200

    New migrations

--- Todo.Api/Migrations/20241019170924_DescriptionNullable.Designer.cs
+++ Todo.Api/Migrations/20241019170924_DescriptionNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Todo.Api.Data;
 
@@ -11,9 +12,11 @@ using Todo.Api.Data;
 namespace Todo.Api.Migrations
 {
     [DbContext(typeof(TodoDbContext))]
-    partial class TodoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241019170924_DescriptionNullable")]
+    partial class DescriptionNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

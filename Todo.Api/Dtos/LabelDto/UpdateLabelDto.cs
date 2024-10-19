91dec829d94adc6bedd76b870bfa0a37613503f1
Zigalow <s224302@dtu.dk>
2024-10-19 17:48:40 +0200

    Made dtos for Label

---
+++ Todo.Api/Dtos/LabelDto/UpdateLabelDto.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Todo.Api.Dtos.LabelDto;
+
+public class UpdateLabelDto
+{
+    public string Name { get; set; } = string.Empty;
+
+    [StringLength(7)]
+    [RegularExpression("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$")]
+    public string? Color { get; set; } = null;
+}

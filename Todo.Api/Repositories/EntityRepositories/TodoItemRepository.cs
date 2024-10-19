2cd8c0a261afe4c8e3876deb587f5716adccbfd5
Zigalow <s224302@dtu.dk>
2024-10-19 18:35:06 +0200

    Refactored crud methods to include labels when fetching

--- Todo.Api/Repositories/EntityRepositories/TodoItemRepository.cs
+++ Todo.Api/Repositories/EntityRepositories/TodoItemRepository.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Todo.Api.Data;
-using Todo.Api.Dtos.TodoItemDtos;
 using Todo.Api.Interfaces.EntityInterfaces;
+using Todo.Core.Common;
 using Todo.Core.Entities;
 
 namespace Todo.Api.Repositories.EntityRepositories;
@@ -15,20 +15,32 @@ public class TodoItemRepository : GenericRepository<TodoItem>, ITodoItemReposito
         _dbContext = dbContext;
     }
 
+    public new async Task<List<TodoItem>> GetAllAsync()
+    {
+        return await _dbContext.TodoItems
+            .Include(i => i.Labels)
+            .ToListAsync();
+    }
+
     public async Task<List<TodoItem>?> GetAllTodoItemsForTodoList(int todoListId)
     {
         var todoItemsFromList = await _dbContext.TodoLists
             .Where(tl => tl.Id == todoListId)
-            .Select(tl => new { tl.Items })
+            .Include(tl => tl.Items)
+            .ThenInclude(i => i.Labels)
+            .Select(tl => tl.Items)
             .FirstOrDefaultAsync();
 
-        return todoItemsFromList?.Items.ToList();
+        return todoItemsFromList?.ToList();
     }
 
     public async Task<List<TodoItem>?> GetAllTodoItemsForProject(int projectId)
     {
         var todoItemsFromProject = await _dbContext.Projects
             .Where(p => p.Id == projectId)
+            .Include(p => p.TodoLists)
+            .ThenInclude(tl => tl.Items)
+            .ThenInclude(i => i.Labels)
             .SelectMany(p => p.TodoLists)
             .Select(tl => new { tl.Items })
             .FirstOrDefaultAsync();

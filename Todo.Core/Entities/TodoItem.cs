def1b39c4c61b3b454ae0c265ba01cff1abedf63
Zigalow <s224302@dtu.dk>
2024-10-19 19:16:21 +0200

    Made description nullable

--- Todo.Core/Entities/TodoItem.cs
+++ Todo.Core/Entities/TodoItem.cs
@@ -13,7 +13,7 @@ public class TodoItem
     public string Title { get; set; } = string.Empty;
 
     [StringLength(500)]
-    public string Description { get; set; } = string.Empty;
+    public string? Description { get; set; } = string.Empty;
 
     public DateTime? DueDate { get; set; }
 

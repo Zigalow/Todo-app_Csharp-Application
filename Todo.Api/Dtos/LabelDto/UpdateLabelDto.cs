de186e24871c8b8584bccb4dfb97e8f5562509cf
Zigalow <s224302@dtu.dk>
2024-10-19 18:45:35 +0200

    Updated ErrorMessage for color validation

--- Todo.Api/Dtos/LabelDto/UpdateLabelDto.cs
+++ Todo.Api/Dtos/LabelDto/UpdateLabelDto.cs
@@ -7,6 +7,8 @@ public class UpdateLabelDto
     public string Name { get; set; } = string.Empty;
 
     [StringLength(7)]
-    [RegularExpression("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$")]
+    [RegularExpression("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$",
+        ErrorMessage = "Color must be a valid hex color code. Did you remember to include the '#'?")]
+
     public string? Color { get; set; } = null;
 }

3ec5ebe0c33dec377c964c536e953ec8a96c9a09
Zaid Sheikh <Zaidsh02@gmail.com>
2024-10-24 22:27:49 +0200

    Removed Unnecessary
    Co-Authored-By: Zacharias Egholm <113907743+Zigalow@users.noreply.github.com>

--- Todo.Web/Components/TodoComponentClass/Project.cs
+++
@@ -1,8 +0,0 @@
-namespace Todo.Web.Components.TodoComponent
-{
-    public class Project{
-        public required int Id { get; set; }
-        public required string Name { get; set; }
-        public List<TodoList>? TodoLists { get; set; }
-    }
-}

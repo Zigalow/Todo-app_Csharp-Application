b1b34a517dfa3a21b3f55cf20a13a6ec4ce50ee9
Zaid Sheikh <Zaidsh02@gmail.com>
2024-10-27 23:33:56 +0100

    Connected to database

--- Todo.Web/Program.cs
+++ Todo.Web/Program.cs
@@ -1,10 +1,10 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Todo.Api.Data;
 using Todo.Web.Components;
 using Todo.Web.Components.Account;
-using Todo.Web.Data;
-//using Todo.Core.Entities;
+using Todo.Core.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,57 +12,42 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddCascadingAuthenticationState();
+
 builder.Services.AddScoped<IdentityUserAccessor>();
 builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
 
-builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultScheme = IdentityConstants.ApplicationScheme;
-        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-        
-    })
+builder.Services.AddAuthentication()
     .AddCookie(options =>
-        {
-            options.LoginPath = "/Account/Unauthorized/";
-            options.AccessDeniedPath = "/Account/Forbidden/";
-        })
-    .AddIdentityCookies();
-    /*.AddGoogle(options =>
     {
-        options.ClientId = "YOUR_GOOGLE_CLIENT_ID";
-        options.ClientSecret = "YOUR_GOOGLE_CLIENT_SECRET";
-    });*/
+        options.LoginPath = "/Account/Unauthorized/";
+        options.AccessDeniedPath = "/Account/Forbidden/";
+    });
 
-builder.Services.AddIdentityCore<ApplicationUser>(options => //TODO: Use the correct ApplicationUser
-{
-    options.SignIn.RequireConfirmedAccount = true;
-})
-//.AddRoles<ApplicationRole>()   //TODO: Use the correct ApplicationRole
-.AddEntityFrameworkStores<ApplicationDbContext>()
-.AddSignInManager()
-.AddDefaultTokenProviders();
+builder.Services.AddAuthorization();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
-                       throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+builder.Services.AddCascadingAuthenticationState();
+
+// Add Identity configuration
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+    {
+        options.SignIn.RequireConfirmedAccount = false; /*TODO: Change to True, when conformation is in place*/
+    })
+    .AddEntityFrameworkStores<TodoDbContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders()
+    .AddApiEndpoints();
+
+
+//Connect with todoApi
+builder.Services.AddHttpClient("TodoApi", client => { client.BaseAddress = new Uri("http://localhost:5118"); });
+
+builder.Services.AddDbContext<TodoDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddSignInManager()
-    .AddDefaultTokenProviders();
-
-builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
-
-/*builder.Services.AddRazorPages(options =>
-{
-    options.Conventions.AuthorizePage("/");
-    options.Conventions.AuthorizePage("/counter");
-    options.Conventions.AllowAnonymousToPage("/Account/Login");
-});*/
 
 var app = builder.Build();
 
@@ -74,21 +59,20 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-app.UseAntiforgery();
+
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
-// Add additional endpoints required by the Identity /Account Razor components.
 app.MapAdditionalIdentityEndpoints();
-
 app.UseAuthentication();
 app.UseAuthorization();
+app.UseAntiforgery();
+app.MapIdentityApi<ApplicationUser>();
 
 app.Run();

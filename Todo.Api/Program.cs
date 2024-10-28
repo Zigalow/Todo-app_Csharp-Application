66460805282a0f92ca3069a0aa7f41ee318fd4bd
Zigalow <s224302@dtu.dk>
2024-10-28 23:36:11 +0100

    Added scope of ICurrentUserService and HttpContextAccessor

--- Todo.Api/Program.cs
+++ Todo.Api/Program.cs
@@ -3,11 +3,14 @@ using Todo.Api.Data;
 using Todo.Api.Interfaces;
 using Todo.Api.Repositories;
 using Todo.Api.Repositories.Interfaces;
+using Todo.Api.Services;
+using Todo.Api.Services.interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,6 +22,7 @@ builder.Services.AddDbContext<TodoDbContext>(options =>
 // Register UnitOfWork
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
 
 var app = builder.Build();
 

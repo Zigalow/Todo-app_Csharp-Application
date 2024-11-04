01b762608c3d380bcaffd99ee3c6676b7f334af3
Zaid Sheikh <Zaidsh02@gmail.com>
2024-11-04 13:15:59 +0100

    Added the missing variable

--- Todo.Web/Auth/Models/RegisterRequest.cs
+++ Todo.Web/Auth/Models/RegisterRequest.cs
@@ -5,4 +5,5 @@ public class RegisterRequest
     public string Username { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
+    public string ConfirmPassword { get; set; } = string.Empty;
 }

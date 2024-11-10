b27dea066870ca926fc97ade0804eb5f3c1e00d5
Zigalow <s224302@dtu.dk>
2024-11-10 20:38:45 +0100

    Added CanCreate.. methods for interface and modified some exisiting ones

--- Todo.Api/Repositories/Interfaces/IAuthorizationRepository.cs
+++ Todo.Api/Repositories/Interfaces/IAuthorizationRepository.cs
@@ -3,11 +3,15 @@ namespace Todo.Api.Repositories.Interfaces;
 public interface IAuthorizationRepository
 {
     Task<bool> CanAccessProjectAsync(string userId, int projectId);
+    Task<bool> CanCreateProjectAsync(string userId);
     Task<bool> CanModifyProjectAsync(string userId, int projectId);
     Task<bool> CanAccessTodoListAsync(string userId, int todoListId);
+    Task<bool> CanCreateTodoListAsync(string userId, int projectId);
     Task<bool> CanModifyTodoListAsync(string userId, int todoListId);
     Task<bool> CanAccessTodoItemAsync(string userId, int todoItemId);
+    Task<bool> CanCreateTodoItemAsync(string userId, int todoListId);
     Task<bool> CanModifyTodoItemAsync(string userId, int todoItemId);
-    Task<bool> CanAccessTodoLabelAsync(string userId, int labelId);
-    Task<bool> CanModifyTodoLabelAsync(string userId, int labelId);
+    Task<bool> CanAccessLabelAsync(string userId, int labelId);
+    Task<bool> CanCreateLabelAsync(string userId, int projectId);
+    Task<bool> CanModifyLabelAsync(string userId, int labelId);
 }

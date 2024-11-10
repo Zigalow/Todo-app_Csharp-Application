99bd6e97448e6e983f0b22d0e313d8ee9a412ec4
Zigalow <s224302@dtu.dk>
2024-11-10 03:30:40 +0100

    Added initial interface and class for TodoListService.cs

--- Todo.Web/Services/TodoListService.cs
+++ Todo.Web/Services/TodoListService.cs
@@ -1,8 +1,33 @@
+using Todo.Core.Dtos.TodoListDtos;
+using Todo.Core.Entities;
 using Todo.Web.Services.interfaces;
 
 namespace Todo.Web.Services;
 
 public class TodoListService:ITodoListService
 {
-    
+    public Task<List<TodoList>> GetAllTodoListsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TodoList?> GetTodoListByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> CreateTodoListAsync(CreateTodoListDto todoListDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateTodoListAsync(int id, UpdateTodoListDto todoList)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteTodoListAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
 }
--- Todo.Web/Services/interfaces/ITodoListService.cs
+++ Todo.Web/Services/interfaces/ITodoListService.cs
@@ -1,6 +1,13 @@
+using Todo.Core.Dtos.TodoListDtos;
+using Todo.Core.Entities;
+
 namespace Todo.Web.Services.interfaces;
 
 public interface ITodoListService
 {
-    
+    Task<List<TodoList>> GetAllTodoListsAsync();
+    Task<TodoList?> GetTodoListByIdAsync(int id);
+    Task<bool> CreateTodoListAsync(CreateTodoListDto todoListDto);
+    Task<bool> UpdateTodoListAsync(int id, UpdateTodoListDto todoList);
+    Task<bool> DeleteTodoListAsync(int id);
 }

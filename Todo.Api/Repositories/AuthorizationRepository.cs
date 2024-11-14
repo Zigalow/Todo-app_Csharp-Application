d63fee5fc2c537fda4c6a4a864fecc38f31b517e
Zigalow <s224302@dtu.dk>
2024-11-14 01:42:31 +0100

    Added IsAdmin method to check if the user is an admin, and added them to the methods

--- Todo.Api/Repositories/AuthorizationRepository.cs
+++ Todo.Api/Repositories/AuthorizationRepository.cs
@@ -13,8 +13,16 @@ public class AuthorizationRepository : IAuthorizationRepository
         _dbContext = dbContext;
     }
 
+    public async Task<bool> IsAdminAsync(string userId)
+    {
+        return await _dbContext.Projects.AnyAsync(p => p.AdminId == userId);
+    }
+
     public async Task<bool> CanAccessProjectAsync(string userId, int projectId)
     {
+        if (await IsAdminAsync(userId))
+            return true;
+
         return await _dbContext.Projects
             .AnyAsync(p => p.Id == projectId &&
                            (p.AdminId == userId || p.Collaborators.Any(c => c.UserId == userId)));
@@ -28,10 +36,15 @@ public class AuthorizationRepository : IAuthorizationRepository
     public Task<bool> CanModifyProjectAsync(string userId, int projectId)
     {
         throw new NotImplementedException();
+        if (await IsAdminAsync(userId))
+            return true;
     }
 
     public async Task<bool> CanAccessTodoListAsync(string userId, int todoListId)
     {
+        if (await IsAdminAsync(userId))
+            return true;
+
         return await _dbContext.TodoLists
             .AnyAsync(tl => tl.Id == todoListId &&
                             (tl.Project.AdminId == userId ||
@@ -41,15 +54,22 @@ public class AuthorizationRepository : IAuthorizationRepository
     public Task<bool> CanCreateTodoListAsync(string userId, int projectId)
     {
         throw new NotImplementedException();
+        if (await IsAdminAsync(userId))
+            return true;
     }
 
     public Task<bool> CanModifyTodoListAsync(string userId, int todoListId)
     {
         throw new NotImplementedException();
+        if (await IsAdminAsync(userId))
+            return true;
     }
 
     public async Task<bool> CanAccessTodoItemAsync(string userId, int todoItemId)
     {
+        if (await IsAdminAsync(userId))
+            return true;
+
         return await _dbContext.TodoItems
             .AnyAsync(ti => ti.Id == todoItemId &&
                             (ti.TodoList.Project.AdminId == userId ||
@@ -59,15 +79,21 @@ public class AuthorizationRepository : IAuthorizationRepository
     public Task<bool> CanCreateTodoItemAsync(string userId, int todoListId)
     {
         throw new NotImplementedException();
+        if (await IsAdminAsync(userId))
+            return true;
     }
 
     public Task<bool> CanModifyTodoItemAsync(string userId, int todoItemId)
     {
         throw new NotImplementedException();
+        if (await IsAdminAsync(userId))
+            return true;
     }
 
     public async Task<bool> CanAccessLabelAsync(string userId, int labelId)
     {
+        if (await IsAdminAsync(userId))
+            return true;
         return await _dbContext.Labels
             .AnyAsync(l => l.Id == labelId &&
                            (l.Project.AdminId == userId ||
@@ -77,10 +103,16 @@ public class AuthorizationRepository : IAuthorizationRepository
     public Task<bool> CanCreateLabelAsync(string userId, int projectId)
     {
         throw new NotImplementedException();
+        if (await IsAdminAsync(userId))
+            return true;
+        
     }
 
     public Task<bool> CanModifyLabelAsync(string userId, int labelId)
     {
         throw new NotImplementedException();
+        if (await IsAdminAsync(userId))
+            return true;
+
     }
 }

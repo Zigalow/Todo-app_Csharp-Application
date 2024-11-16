3842aca388f8bfcf85b0579c78ba0c5a550febdc
Zigalow <s224302@dtu.dk>
2024-11-16 16:36:42 +0100

    Added repository for ProjectCollaborator and moved methods from Project repository to this one

--- Todo.Api/Repositories/ProjectRepository.cs
+++ Todo.Api/Repositories/ProjectRepository.cs
@@ -14,37 +14,6 @@ public class ProjectRepository : GenericRepository<Project>, IProjectRepository
         _dbContext = dbContext;
     }
 
-    public async Task<IEnumerable<ApplicationUser>> GetProjectCollaboratorsAsync(int projectId)
-    {
-        return await _dbContext.ProjectCollaborators
-            .Where(c => c.ProjectId == projectId)
-            .Select(c => c.ApplicationUser)
-            .ToListAsync();
-    }
-
-    public async Task AddCollaboratorAsync(int projectId, string userId, ProjectRole role)
-    {
-        var collaborator = new ProjectCollaborators
-        {
-            ProjectId = projectId,
-            UserId = userId,
-            Role = role
-        };
-
-        await _dbContext.ProjectCollaborators.AddAsync(collaborator);
-    }
-
-    public async Task RemoveCollaboratorAsync(int projectId, int userId)
-    {
-        var collaborator = await _dbContext.ProjectCollaborators
-            .FirstOrDefaultAsync(c => c.ProjectId == projectId && c.UserId.Equals(userId));
-
-        if (collaborator != null)
-        {
-            _dbContext.ProjectCollaborators.Remove(collaborator);
-        }
-    }
-
     public override async Task<IEnumerable<Project>> GetAllAsync(string userId)
     {
         return await _dbContext.Projects

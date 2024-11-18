d9f32ae65b34278bdda048aa1c274b94d2b7fba6
Zaid Sheikh <Zaidsh02@gmail.com>
2024-11-18 01:52:58 +0100

    Removed unesseory varables

--- Todo.Core/Entities/TodoItem.cs
+++ Todo.Core/Entities/TodoItem.cs
@@ -27,12 +27,6 @@ public class TodoItem
 
     [ForeignKey(nameof(TodoListId))]
     public TodoList TodoList { get; set; } = null!;
-
-        [NotMapped]
-        public bool IsDropdownVisible { get; set; }
-        
-        [NotMapped]
-        public bool IsLabelEditorVisible { get; set; }
     public ICollection<Label> Labels { get; set; } = new List<Label>();
     // public ICollection<ApplicationUser> Assignees { get; set; } = new List<ApplicationUser>();
 }

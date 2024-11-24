24b8cf82b11a3471cb197e8f4a91a575c8e3e5ba
Zigalow <s224302@dtu.dk>
2024-11-25 00:39:31 +0100

    Added AuthResult so that errorMessage from api can be used in frontend

--- Todo.Web/Auth/IAuthService.cs
+++ Todo.Web/Auth/IAuthService.cs
@@ -5,7 +5,7 @@ namespace Todo.Web.Auth;
 public interface IAuthService
 {
     Task<bool> LoginAsync(LoginRequest request);
-    Task<bool> RegisterAsync(RegisterRequest request);
+    Task<AuthResult> RegisterAsync(RegisterRequest request);
     Task LogoutAsync();
     Task<bool> IsAuthenticatedAsync();
 }

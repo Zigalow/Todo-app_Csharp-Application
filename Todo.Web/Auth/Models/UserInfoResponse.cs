f903d908c57822b088178bb7455f541acad4c843
Zaid Sheikh <Zaidsh02@gmail.com>
2024-12-09 20:59:09 +0100

    Fixed the bug

--- Todo.Web/Auth/Models/UserInfoResponse.cs
+++ Todo.Web/Auth/Models/UserInfoResponse.cs
@@ -2,6 +2,7 @@ namespace Todo.Web.Auth.Models;
 
 public class UserInfoResponse
 {
+    public string Id { get; set; } = string.Empty;
     public string Username { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string? PhoneNumber { get; set; } = string.Empty;
